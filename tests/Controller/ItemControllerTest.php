cb98a9e65434695fd451c9ae03e9cd4dc004d425
Maarten Bicknese <maarten.bicknese@gmail.com>
2017-02-14 17:50:01 +0100

    Stop testing for twig rendering

--- tests/Controller/ItemControllerTest.php
+++ tests/Controller/ItemControllerTest.php
@@ -1,12 +1,7 @@
 <?php
-
 namespace App\Tests\Controller;
 
-use App\Controller\ItemController;
-use App\Entity\Item;
-use App\Repository\ItemRepository;
 use App\Tests\BaseTestCase;
-use Symfony\Component\DomCrawler\Crawler;
 
 class ItemControllerTest extends BaseTestCase
 {
@@ -19,32 +14,5 @@ class ItemControllerTest extends BaseTestCase
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals(1, $crawler->filter('h1:contains("Hello World")')->count());
-    }
-
-    public function testItemListIsRendered()
-    {
-        self::bootKernel();
-        $twig = self::$kernel->getContainer()->get('twig');
-        $repository = new class extends ItemRepository {
-            public function __construct(){}
-            public function findAll()
-            {
-                return [
-                    Item::fromName('Item 1'),
-                    Item::fromName('Item 2'),
-                    Item::fromName('Item 3'),
-                    Item::fromName('Item 4'),
-                ];
-            }
-        };
-        $controller = new ItemController($twig, $repository);
-        $response = $controller->index();
-        $crawler = new Crawler($response->getContent());
-        $nodeList = $crawler->filter('ul>li');
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(4, $nodeList->count());
-        $this->assertEquals('Item 1', $nodeList->first()->text());
-        $this->assertEquals('Item 4', $nodeList->last()->text());
     }
 }
