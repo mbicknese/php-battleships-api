61f3337912071deab99f1f038efe43f0e5497f12
Maarten Bicknese <maarten.bicknese@gmail.com>
2017-04-07 14:06:17 +0200

    Create a route for the handler

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Tests;
 
+use App\Model\Match\Match;
+use App\Model\Match\MatchId;
+use App\Uid64\Uid64;
 use Symfony\Bundle\FrameworkBundle\Client;
 
 /**
@@ -14,17 +17,30 @@ class RouteTest extends BaseTestCase
     /**
      * @var Client
      */
-    private $client;
+    private static $client;
 
     public function setUp()
     {
-        $this->client = self::createClient();
+        self::$client = self::createClient();
         self::createSchema();
     }
 
+    /**
+     * @return mixed
+     */
     public function testJoin()
     {
-        $this->client->request('POST', '/match');
-        $this->assertEquals(201, $this->client->getResponse()->getStatusCode());
+        self::$client->request('POST', '/match');
+        $this->assertEquals(201, self::$client->getResponse()->getStatusCode());
+        $id = json_decode(self::$client->getResponse()->getContent(), true)['id'];
+        return $id;
+    }
+
+    public function testDisplay()
+    {
+        $match = new Match(new MatchId());
+        self::$kernel->getContainer()->get('app.repository.match')->persist($match);
+        self::$client->request('GET', '/match/' . Uid64::toText($match->id()));
+        $this->assertEquals(200, self::$client->getResponse()->getStatusCode());
     }
 }
